a0e864014ed6be79fafa62c646cb2e65fc1a0204
snh15978 <snh15978@gmail.com>
2022-10-03 00:15:24 +0900

    style: formatting RandomBallNumberGenerator

--- src/main/java/baseball/domain/RandomBallNumberGenerator.java
+++ src/main/java/baseball/domain/RandomBallNumberGenerator.java
@@ -7,7 +7,6 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomBallNumberGenerator {
     private static final int TOTAL_NUMBER_COUNT = 3;
-
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 9;
 

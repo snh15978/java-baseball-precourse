fe62f704097f33bf7a74d6c83ae1482e4b85fa61
snh15978 <snh15978@gmail.com>
2022-10-02 20:52:01 +0900

    test: 입력 값에 따라 결과가 다른경우에 대한 테스트 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,12 +3,14 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
 
@@ -37,5 +39,18 @@ public class SetTest {
         numberSet.add(3);
 
         assertThat(numberSet.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void set_contains_test2(int input, boolean expect) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        numberSet.add(1);
+        numberSet.add(1);
+        numberSet.add(2);
+        numberSet.add(3);
+        assertEquals(numberSet.contains(input), expect);
     }
 }
